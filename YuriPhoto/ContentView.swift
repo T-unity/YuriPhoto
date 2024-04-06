7dc2da6dab4bd072c3a6c98cf53186728dc241f4
T-unity <takadanaritoshi@gmail.com>
2024-04-06 23:59:43 +0900

    Initial Commit

---
+++ YuriPhoto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YuriPhoto
+//
+//  Created by 高田斉賢 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YuriPhoto/YuriPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuriPhotoApp.swift
+//  YuriPhoto
+//
+//  Created by 高田斉賢 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct YuriPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
