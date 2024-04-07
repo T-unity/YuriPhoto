0626059681d0b2cd43a690571f18720683844e4b
T-unity <takadanaritoshi@gmail.com>
2024-04-07 13:02:03 +0900

    WIP

--- YuriPhoto/ContentView.swift
+++ YuriPhoto/ContentView.swift
@@ -1,26 +1,63 @@
-//
-//  ContentView.swift
-//  YuriPhoto
-//
-//  Created by 高田斉賢 on 2024/04/06.
-//
-
 import SwiftUI
+import UIKit
 
+// UIImagePickerControllerを使用するためのラッパークラス
+struct CustomImagePicker: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    @Binding var selectedImage: UIImage?
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: CustomImagePicker
+        
+        init(_ parent: CustomImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.isPresented = false
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isPresented = false
+        }
+    }
+}
+
+// SwiftUIビュー
 struct ContentView: View {
+    @State private var isShowingImagePicker = false
+    @State private var image: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Button("写真を撮る") {
+                isShowingImagePicker = true
+            }
+            .sheet(isPresented: $isShowingImagePicker) {
+                CustomImagePicker(isPresented: $isShowingImagePicker, selectedImage: $image)
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
