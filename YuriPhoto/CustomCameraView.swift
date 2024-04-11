d8dc7a5f1c5fd6034b482d2c097fb63188b661cc
T-unity <takadanaritoshi@gmail.com>
2024-04-12 02:31:41 +0900

    写真撮影後、プレビューを表示

--- YuriPhoto/CustomCameraView.swift
+++ YuriPhoto/CustomCameraView.swift
@@ -7,19 +7,31 @@ struct CustomCameraView: UIViewRepresentable {
         private let photoOutput = AVCapturePhotoOutput()
         private var captureButton: UIButton?
         private var capturedImage: UIImage?
+        private var imageView: UIImageView?
+        private var previewLayer: AVCaptureVideoPreviewLayer?
         
         override init(frame: CGRect) {
             super.init(frame: frame)
             initializeSession()
             setupCaptureButton()
+            setupImageView()
         }
         
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
         
+        private func setupImageView() {
+            imageView = UIImageView(frame: self.bounds)
+            imageView?.contentMode = .scaleAspectFit
+            imageView?.isHidden = true
+            if let imageView = imageView {
+                self.addSubview(imageView)
+//                print("ImageView added")  // デバッグ情報
+            }
+        }
+
         private func setupCaptureButton() {
-            // 撮影ボタンを初期化して追加
             captureButton = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
             if let captureButton = captureButton {
                 captureButton.backgroundColor = .white
@@ -28,15 +40,18 @@ struct CustomCameraView: UIViewRepresentable {
                 captureButton.setTitleColor(.black, for: .normal)
                 captureButton.addTarget(self, action: #selector(takePhoto), for: .touchUpInside)
                 self.addSubview(captureButton)
+                self.bringSubviewToFront(captureButton)
             }
         }
         
         override func layoutSubviews() {
             super.layoutSubviews()
+            imageView?.frame = self.bounds  // imageViewのフレームを更新
             captureButton?.center = CGPoint(x: self.bounds.midX, y: self.bounds.height - 100)
             self.bringSubviewToFront(captureButton!)
         }
-        
+
+
         private func initializeSession() {
             DispatchQueue.main.async {
                 self.captureSession = AVCaptureSession()
@@ -53,48 +68,80 @@ struct CustomCameraView: UIViewRepresentable {
                     session.addOutput(self.photoOutput)
                 }
                 
-                let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+                self.previewLayer = AVCaptureVideoPreviewLayer(session: session)
+                guard let previewLayer = self.previewLayer else { return }
+                
                 previewLayer.frame = self.bounds
                 previewLayer.videoGravity = .resizeAspectFill
-                self.layer.addSublayer(previewLayer)
+                DispatchQueue.main.async {
+                    self.layer.addSublayer(previewLayer)
+                }
                 
                 session.startRunning()
             }
         }
-        
+
         @objc func takePhoto() {
             let settings = AVCapturePhotoSettings()
             photoOutput.capturePhoto(with: settings, delegate: self)
         }
-        
+
         func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-            guard let imageData = photo.fileDataRepresentation() else { return }
-            self.capturedImage = UIImage(data: imageData)
+            guard let imageData = photo.fileDataRepresentation(),
+                  let image = UIImage(data: imageData) else { return }
+            self.capturedImage = image
             
-            // 撮影後はボタンのタイトルを「ダウンロード」に変更
             DispatchQueue.main.async {
+                self.imageView?.image = image
+                self.imageView?.isHidden = false
+                self.bringSubviewToFront(self.imageView!)
+//                print("Image should be visible now")  // デバッグ情報
+                self.previewLayer?.isHidden = true
                 self.captureButton?.setTitle("ダウンロード", for: .normal)
                 self.captureButton?.removeTarget(self, action: #selector(self.takePhoto), for: .touchUpInside)
                 self.captureButton?.addTarget(self, action: #selector(self.savePhoto), for: .touchUpInside)
             }
         }
-        
+
+        @objc func retakePhoto() {
+            DispatchQueue.main.async {
+                self.imageView?.isHidden = true
+                self.previewLayer?.isHidden = false
+                self.captureSession?.startRunning()
+                self.captureButton?.setTitle("撮影", for: .normal)
+                self.captureButton?.removeTarget(self, action: #selector(self.retakePhoto), for: .touchUpInside)
+                self.captureButton?.addTarget(self, action: #selector(self.takePhoto), for: .touchUpInside)
+            }
+        }
+
+        @objc func hideImage() {
+            DispatchQueue.main.async {
+                // 画像を非表示にし、ボタンを再撮影用に設定
+                self.imageView?.isHidden = true
+                self.captureButton?.setTitle("撮影", for: .normal)
+                self.captureButton?.removeTarget(self, action: #selector(self.hideImage), for: .touchUpInside)
+                self.captureButton?.addTarget(self, action: #selector(self.takePhoto), for: .touchUpInside)
+            }
+        }
+
         @objc func savePhoto() {
             guard let imageToSave = capturedImage else { return }
             UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
             
-            // 保存後はボタンを非表示にするか、必要に応じてさらなる処理を行う
             DispatchQueue.main.async {
+                self.imageView?.isHidden = true
+                self.previewLayer?.isHidden = false
+                self.captureSession?.startRunning()
                 self.captureButton?.setTitle("撮影", for: .normal)
                 self.captureButton?.removeTarget(self, action: #selector(self.savePhoto), for: .touchUpInside)
                 self.captureButton?.addTarget(self, action: #selector(self.takePhoto), for: .touchUpInside)
             }
         }
     }
-    
+
     func makeUIView(context: Context) -> CameraUIView {
         return CameraUIView()
     }
-    
+
     func updateUIView(_ uiView: CameraUIView, context: Context) {}
 }
