521bf6441fe9c9b64a941829b5713ac8961f850e
T-unity <takadanaritoshi@gmail.com>
2024-04-21 22:37:32 +0900

    CameraUIViewクラスを別ファイルに移動

--- YuriPhoto/CameraManager.swift
+++ YuriPhoto/CameraManager.swift
@@ -1,8 +1,75 @@
+//import SwiftUI
+//import AVFoundation
 //
-//  CameraManager.swift
-//  YuriPhoto
+//class CameraManager: NSObject {
+//    var session: AVCaptureSession
+//    var photoOutput: AVCapturePhotoOutput
+//    var previewLayer: AVCaptureVideoPreviewLayer?
+//    
+//    override init() {
+//        self.session = AVCaptureSession()
+//        self.photoOutput = AVCapturePhotoOutput()
+//        super.init()
+//        self.setupSession()
+//    }
+//    
+//    private func setupSession() {
+//        session.beginConfiguration()
+//        
+//        guard let backCamera = AVCaptureDevice.default(for: .video),
+//              let input = try? AVCaptureDeviceInput(device: backCamera) else {
+//            fatalError("Unable to access back camera")
+//        }
+//        
+//        if session.canAddInput(input) {
+//            session.addInput(input)
+//        } else {
+//            fatalError("Cannot add input to the session")
+//        }
+//        
+//        if session.canAddOutput(photoOutput) {
+//            session.addOutput(photoOutput)
+//        } else {
+//            fatalError("Cannot add output to the session")
+//        }
+//        
+//        session.commitConfiguration()
+//        setupPreviewLayer()
+//    }
+//    
+//    private func setupPreviewLayer() {
+//        self.previewLayer = AVCaptureVideoPreviewLayer(session: session)
+//        self.previewLayer?.videoGravity = .resizeAspectFill
+//    }
+//    
+//    func startSession() {
+//        if !session.isRunning {
+//            session.startRunning()
+//        }
+//    }
+//    
+//    func stopSession() {
+//        if session.isRunning {
+//            session.stopRunning()
+//        }
+//    }
+//    
+//    func capturePhoto(delegate: AVCapturePhotoCaptureDelegate) {
+//        let settings = AVCapturePhotoSettings()
+//        if session.isRunning {
+//            photoOutput.capturePhoto(with: settings, delegate: delegate)
+//        } else {
+//            print("Session is not running")
+//        }
+//    }
+//}
 //
-//  Created by 高田斉賢 on 2024/04/21.
-//
-
-import Foundation
+//extension CameraManager: AVCapturePhotoCaptureDelegate {
+//    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+//        if let error = error {
+//            print("Error capturing photo: \(error.localizedDescription)")
+//        } else {
+//            print("Photo captured successfully")
+//        }
+//    }
+//}
